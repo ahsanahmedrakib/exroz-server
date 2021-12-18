7d9247206e9f9b8c7b62945a92a1cef00c288724
Ahsan Ahmed Rakib <you@example.com>
2021-12-18 23:06:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xdeet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("exroz");
+    const servicesCollection = database.collection("services");
+    const usersCollection = database.collection("users");
+
+    app.post("/services", async (req, res) => {
+      const services = req.body;
+      const result = await servicesCollection.insertOne(services);
+      res.json(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updatedoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updatedoc)
+      res.json(result)
+    });
+
+    //check if a user is admin or not
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+  } finally {
+    // await client.close()
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Exroz");
+});
+
+app.listen(port, () => {
+  console.log(`Exroz listening at http://localhost:${port}`);
+});
